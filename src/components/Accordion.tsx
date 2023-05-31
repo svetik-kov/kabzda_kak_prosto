b08c886eb9df8c87a2bf1c72aa43aaa3d51a4249
Svetlana_Kovalevich <svetik14-06@mail.ru>
2023-05-31 22:16:04 +0300

    Rating and Accordion

---
+++ src/components/Accordion.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+type AccordionType={
+    title:string
+    collapsed:boolean
+}
+export function Accordion(props:AccordionType) {
+    if (props.collapsed===true){
+        return (
+            <div>
+                <AccordionTitle title={props.title} />
+            </div>
+        );
+    } else {
+        return (
+            <div>
+                <AccordionTitle title={props.title} />
+                <AccordionBody />
+            </div>
+        );
+    }
+
+
+}
+
+type AccordionTitleType={
+    title:string
+}
+function AccordionTitle(props:AccordionTitleType) {
+    return <h1> {props.title}</h1>
+}
+
+
+function AccordionBody() {
+    return (
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    )
+}
